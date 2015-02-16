a86d37b7199e6be122e4a45bd955b7ffe81037a1
Steven Muschler <musch711@gmail.com>
2015-02-16 13:11:42 -0600

    Moved ImpermanentDatabase configuration for Unit Tests to AbstractDBTest.  Allows rapid development of DB based Unit Tests.

---
+++ src/test/java/com/unittest/AbstractDBTest.java
@@ -0,0 +1,30 @@
+package com.unittest;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import com.db.DataManager;
+import com.db.UserDB;
+import com.exceptions.DuplicateUserException;
+
+public abstract class AbstractDBTest {
+	protected static String testUsername="testUsername";
+	protected static String testPassword="testPassword";
+	protected static String invalidUsername="foo";
+	
+	@BeforeClass
+	public static void prepareTestDatabase() {
+	    DataManager.initTestDatabase();
+		
+	    try {
+			UserDB.registerUser(testUsername, testPassword);
+		} catch (DuplicateUserException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public static void destroyTestDatabase() {
+	    DataManager.shutdownGraphDb();
+	}
+}
--- src/test/java/com/unittest/db/UserDBTest.java
+++ src/test/java/com/unittest/db/UserDBTest.java
@@ -2,37 +2,16 @@ package com.unittest.db;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import com.db.DataManager;
+
 import com.db.UserDB;
 import com.exceptions.DuplicateUserException;
 import com.exceptions.UserNotAuthorizedException;
 import com.exceptions.UserNotFoundException;
 import com.objects.domain.User;
+import com.unittest.AbstractDBTest;
 
-public class UserDBTest {
-	private static String testUsername="testUsername";
-	private static String testPassword="testPassword";
-	private static String invalidUsername="foo";
-	
-	@BeforeClass
-	public static void prepareTestDatabase() {
-	    DataManager.initTestDatabase();
-		
-	    try {
-			UserDB.registerUser(testUsername, testPassword);
-		} catch (DuplicateUserException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@AfterClass
-	public static void destroyTestDatabase() {
-	    DataManager.shutdownGraphDb();
-	}
-	
+public class UserDBTest extends AbstractDBTest {
 	@Test
 	public void userExistsTest() {
 		boolean result = UserDB.userExists(testUsername);
