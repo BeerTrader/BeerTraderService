6d0b8be5f1765c9c0f40ddc238ea2cacaf4350ac
Steven Muschler <musch711@gmail.com>
2015-03-03 21:21:37 -0600

    Removed Ignore flag on DB tests, since those are using the ImpermanentDatabase from Neo4J.

--- src/test/java/com/unittest/db/UserDBTest.java
+++ src/test/java/com/unittest/db/UserDBTest.java
@@ -13,7 +13,6 @@ import com.objects.domain.User;
 import com.unittest.AbstractDBTest;
 
 public class UserDBTest extends AbstractDBTest {
-	@Ignore
 	@Test
 	public void userExistsTest() {
 		boolean result = UserDB.userExists(testUsername);
@@ -21,34 +20,29 @@ public class UserDBTest extends AbstractDBTest {
 		assertTrue(result);
 	}
 	
-	@Ignore
 	@Test(expected=UserNotFoundException.class)
 	public void getUserFailureTest() throws UserNotFoundException {
 		@SuppressWarnings("unused")
 		User user = UserDB.getUser(invalidUsername);
 	}
 	
-	@Ignore
 	@Test
 	public void getUserSuccessTest() throws UserNotFoundException {
 		User user = UserDB.getUser(testUsername);
 		assertNotNull(user);
 	}
 	
-	@Ignore
 	@Test(expected=UserNotAuthorizedException.class)
 	public void authenticateUserFailureTest() throws UserNotAuthorizedException {
 		UserDB.authenticateUser(invalidUsername, testPassword);
 	}
 	
-	@Ignore
 	@Test
 	public void authenticateUserSuccessTest() throws UserNotAuthorizedException {
 		User user = UserDB.authenticateUser(testUsername, testPassword);
 		assertNotNull(user);
 	}
 	
-	@Ignore
 	@Test(expected=DuplicateUserException.class)
 	public void registerUserFailureTest() throws DuplicateUserException {
 		UserDB.registerUser(testUsername, testPassword);
